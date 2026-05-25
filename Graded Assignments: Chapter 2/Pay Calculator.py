160971a23d2ef15138f9f23d6dce61fdc5349f0e
Aadit Agarwal <aaditag9983@gmail.com>
2026-05-25 15:07:49 +0530

    Graded Assignment 2

---
+++ Graded Assignments: Chapter 2/Pay Calculator.py
@@ -0,0 +1,9 @@
+# Getting the inputs
+hours = input("Enter your working hours:")
+hrs = float(hours)
+money = input("Enter your rate:")
+rate = float(money)
+
+# Calculating the salary
+pay = hrs * rate
+print('Pay:', pay)
